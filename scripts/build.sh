e574c1fc2d579e558e042af367d090d48c274b0d
magicDGS <daniel.gomez.sanchez@hotmail.es>
2023-05-12 20:26:41 +0200

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # remove the previous file
-rm -fr bundle/*.yaml
+rm -fr dist/*.yaml
 # linting
 npx @redocly/cli@latest lint src/ygoprodeck-api.yaml
 # if linting does not fail, bundle it
 if [[ $? == 0 ]]; then
-	npx @redocly/cli@latest bundle src/ygoprodeck-api.yaml -o bundle/ygoprodeck-api.yaml
+	npx @redocly/cli@latest bundle src/ygoprodeck-api.yaml -o dist/ygoprodeck-api.yaml
 else
 	echo "ERROR: invalid OpenAPI definition" >&2
 	exit 1
